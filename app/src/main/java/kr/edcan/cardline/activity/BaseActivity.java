b73bca6ed1dfb22677a64077ea1ab7473fb0cc58
Junseok Oh <kotohana5706@edcan.kr>
2017-04-07 00:33:30 +0900

    [Activity] Build BaseActivity Structure
    * Make onCreateViewId(), onCreateViewToolbarId() abstract

--- app/src/main/java/kr/edcan/cardline/activity/BaseActivity.java
+++ app/src/main/java/kr/edcan/cardline/activity/BaseActivity.java
@@ -1,20 +1,35 @@
 package kr.edcan.cardline.activity;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import kr.edcan.cardline.R;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.setContentView(onCreateViewId());
+        if(onCreateViewToolbarId() != 0) this.setSupportActionBar((Toolbar)findViewById(onCreateViewToolbarId()));
     }
 
-    public abstract int returnLayoutState();
-
-    void setToolbar() {
-
+    @Override
+    public void setContentView(int layoutResId) {
+        super.setContentView(layoutResId);
     }
+
+    protected abstract void setDefault();
+    @LayoutRes
+    protected abstract int onCreateViewId();
+    @IdRes
+    protected abstract int onCreateViewToolbarId();
+
 }
--- app/src/main/java/kr/edcan/cardline/activity/MainActivity.java
+++ app/src/main/java/kr/edcan/cardline/activity/MainActivity.java
@@ -1,16 +1,37 @@
 package kr.edcan.cardline.activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import kr.edcan.cardline.R;
+import kr.edcan.cardline.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected int onCreateViewId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected int onCreateViewToolbarId() {
+        return R.id.toolbar;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 
+    protected void setDefault() {
+        binding = DataBindingUtil.setContentView(this, onCreateViewId());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
