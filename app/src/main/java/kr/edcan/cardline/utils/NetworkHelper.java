e1e37a0529205a720d249e406f8c94c46fbfcf86
Junseok Oh <kotohana5706@edcan.kr>
2017-04-07 01:04:26 +0900

    [Utils] Implement NetworkHelper
    * Return static instance of NetworkAPI
    * Return boolean showing network state

---
+++ app/src/main/java/kr/edcan/cardline/utils/NetworkHelper.java
@@ -0,0 +1,35 @@
+package kr.edcan.cardline.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import java.net.NetworkInterface;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Junseok Oh on 2017-04-07.
+ */
+
+public class NetworkHelper {
+    private final static String url = "http://iwin247.kr";
+    private final static int port = 80;
+
+    private static Retrofit retrofit;
+
+    public static NetworkAPI getNetworkInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(url + ":" + port)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(NetworkAPI.class);
+    }
+
+    public static boolean returnNetworkState(Context context) {
+        ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+}
