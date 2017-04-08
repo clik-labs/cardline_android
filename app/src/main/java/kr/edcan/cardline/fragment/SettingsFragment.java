6a3cac4219b64872aaf903585550f6bff164df59
Junseok Oh <kotohana5706@edcan.kr>
2017-04-09 02:11:40 +0900

    [Fragment] Add Fragments
    * NewsFeedFragment - 뉴스피드
    * MyEditorFragment - 내 에디터 페이지
    * SettingsFragment - 더보기
    * CardlineStudioFragment - 스튜디오

---
+++ app/src/main/java/kr/edcan/cardline/fragment/SettingsFragment.java
@@ -0,0 +1,10 @@
+package kr.edcan.cardline.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Junseok Oh on 2017-04-09.
+ */
+
+public class SettingsFragment extends Fragment {
+}
