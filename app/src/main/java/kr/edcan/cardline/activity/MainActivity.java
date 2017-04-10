44ea1f113624242acc81ab66100711a2ec190a42
Junseok Oh <kotohana5706@edcan.kr>
2017-04-10 12:03:44 +0900

    [Activity] Change MainActivity binding type

--- app/src/main/java/kr/edcan/cardline/activity/MainActivity.java
+++ app/src/main/java/kr/edcan/cardline/activity/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends BaseActivity {
     }
 
     protected void setDefault() {
-        binding = DataBindingUtil.setContentView(this, onCreateViewId());
+        binding = (ActivityMainBinding) baseBinding;
         disableToggle();
     }
 
