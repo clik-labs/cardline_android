d0960da9ad4439e4167c1ddb92d21c34ffdb9e60
Junseok Oh <kotohana5706@edcan.kr>
2017-05-09 19:55:53 +0900

    [View] Remove usued import

--- app/src/main/java/kr/edcan/cardline/views/CartaSquareView.java
+++ app/src/main/java/kr/edcan/cardline/views/CartaSquareView.java
@@ -1,11 +1,8 @@
 package kr.edcan.cardline.views;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.widget.ImageView;
-
-import com.makeramen.roundedimageview.RoundedImageView;
 
 /**
  * Created by Junseok Oh on 2017-05-09.
