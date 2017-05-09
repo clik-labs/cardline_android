8c9eb63aff44bab2ee05ea3f39dd0dec0f82ae93
Junseok Oh <kotohana5706@edcan.kr>
2017-05-09 22:47:54 +0900

    [Activity] Build layout of FavoriteCardlineActivity

--- app/src/main/java/kr/edcan/cardline/activity/FavoriteCardlineActivity.java
+++ app/src/main/java/kr/edcan/cardline/activity/FavoriteCardlineActivity.java
@@ -1,11 +1,35 @@
 package kr.edcan.cardline.activity;
 
+import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.GridLayout;
 
+import com.github.nitrico.lastadapter.BR;
+import com.github.nitrico.lastadapter.LastAdapter;
+import com.github.nitrico.lastadapter.LayoutHandler;
+import com.github.nitrico.lastadapter.Type;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 import kr.edcan.cardline.R;
+import kr.edcan.cardline.databinding.ActivityFavoriteCardlineBinding;
+import kr.edcan.cardline.databinding.MainNewsfeedCommonContentBinding;
+import kr.edcan.cardline.models.CardNews;
 
 public class FavoriteCardlineActivity extends BaseActivity {
+
+    private ActivityFavoriteCardlineBinding binding;
+    private RecyclerView favoriteRecyclerView;
+    private GridLayoutManager layoutManager;
+    private ArrayList<CardNews> favoriteList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +38,48 @@ public class FavoriteCardlineActivity extends BaseActivity {
 
     @Override
     protected void setDefault() {
+        setToolbarTitle("내가 마음에 든 카드 목록");
+        binding = (ActivityFavoriteCardlineBinding) baseBinding;
+        favoriteRecyclerView = binding.favoriteRecyclerView;
+        layoutManager = new GridLayoutManager(this, 2);
+        favoriteRecyclerView.setLayoutManager(layoutManager);
 
+
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+        favoriteList.add(new CardNews("asdf", "asdf", "asdf"));
+
+        LastAdapter.with(favoriteList, BR._all)
+                .map(CardNews.class, new Type<MainNewsfeedCommonContentBinding>(R.layout.main_newsfeed_common_content)
+                        .onBind(new Function1<Type.Params<? extends MainNewsfeedCommonContentBinding>, Unit>() {
+                            @Override
+                            public Unit invoke(Type.Params<? extends MainNewsfeedCommonContentBinding> params) {
+                                return null;
+                            }
+                        })
+                        .onClick(new Function1<Type.Params<? extends MainNewsfeedCommonContentBinding>, Unit>() {
+                            @Override
+                            public Unit invoke(Type.Params<? extends MainNewsfeedCommonContentBinding> params) {
+                                return null;
+                            }
+                        }))
+                .handler(new LayoutHandler() {
+                    @Override
+                    public int getItemLayout(@NotNull Object o, int i) {
+                        return R.layout.main_newsfeed_common_content;
+                    }
+
+                })
+                .into(favoriteRecyclerView);
     }
 
     @Override
@@ -24,6 +89,6 @@ public class FavoriteCardlineActivity extends BaseActivity {
 
     @Override
     protected int onCreateViewToolbarId() {
-        return 0;
+        return R.id.toolbar;
     }
 }
