656f79f607150fb79122219e85c39d8c0dfff424
Junseok Oh <kotohana5706@edcan.kr>
2017-05-09 16:44:05 +0900

    [Models] Add Dummy Model : CardNews.java

---
+++ app/src/main/java/kr/edcan/cardline/models/CardNews.java
@@ -0,0 +1,39 @@
+package kr.edcan.cardline.models;
+
+/**
+ * Created by Junseok Oh on 2017-05-08.
+ */
+
+public class CardNews {
+    private String title, content, imageUrl;
+
+    public CardNews(String title, String content, String imageUrl) {
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
