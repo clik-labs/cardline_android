70f9b44e7c3e22fbed5f9ebc67077c5f7dbe1798
Junseok Oh <kotohana5706@edcan.kr>
2017-05-09 17:55:40 +0900

    [Activity] Replace CardNews UI with CardView * remove RoundImageView Library

--- app/src/main/java/kr/edcan/cardline/views/CartaSquareView.java
+++ app/src/main/java/kr/edcan/cardline/views/CartaSquareView.java
@@ -1,7 +1,9 @@
 package kr.edcan.cardline.views;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 
 import com.makeramen.roundedimageview.RoundedImageView;
 
@@ -9,13 +11,15 @@ import com.makeramen.roundedimageview.RoundedImageView;
  * Created by Junseok Oh on 2017-05-09.
  */
 
-public class CartaSquareView extends RoundedImageView {
+public class CartaSquareView extends ImageView {
     public CartaSquareView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        setMeasuredDimension(width, width);
     }
 }
