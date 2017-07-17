55722a3fd40e39bbc59ee0b4a971a2014bb5d687
Junseok Oh <kotohana5706@edcan.kr>
2017-07-17 23:44:13 +0900

    [Models/User] Add default constructor

--- app/src/main/java/kr/edcan/cardline/models/User.java
+++ app/src/main/java/kr/edcan/cardline/models/User.java
@@ -1,9 +1,71 @@
 package kr.edcan.cardline.models;
 
+import java.util.ArrayList;
+
 /**
  * Created by fluor on 2017-05-10.
  */
 
 public class User {
+    private String email, name, token, profile, profile_img, facebook_id;
 
+    public User() {
+    }
+
+    public User(String email, String name, String token, String profile, String profile_img, String facebook_id) {
+        this.email = email;
+        this.name = name;
+        this.token = token;
+        this.profile = profile;
+        this.profile_img = profile_img;
+        this.facebook_id = facebook_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getProfile_img() {
+        return profile_img;
+    }
+
+    public void setProfile_img(String profile_img) {
+        this.profile_img = profile_img;
+    }
+
+    public String getFacebook_id() {
+        return facebook_id;
+    }
+
+    public void setFacebook_id(String facebook_id) {
+        this.facebook_id = facebook_id;
+    }
 }
